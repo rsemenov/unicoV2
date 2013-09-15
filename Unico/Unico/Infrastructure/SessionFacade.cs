7ce5a899659114fd077b4d19f0b911b060b09baa
ruslan <ruslan@rsemenov.com>
2013-09-15 21:47:05 +0300

    add session extensions

---
+++ Unico/Unico/Infrastructure/SessionFacade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Unico.Infrastructure
+{
+    public static class SessionKeys
+    {
+        public static string ShoppingCardId = "ShoppingCardId";
+    }
+
+    public static class SessionExtensions
+    {
+        public static Guid GetShoppingCardId(this HttpSessionStateBase session)
+        {
+            if (session[SessionKeys.ShoppingCardId] == null)
+            {
+                session[SessionKeys.ShoppingCardId] = Guid.NewGuid();
+            }
+            return (Guid)session[SessionKeys.ShoppingCardId];
+        }
+    }
+}

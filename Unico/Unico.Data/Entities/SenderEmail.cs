3068498e257c15690a3012fe4cd90a3dad6cf3cc
ruslan <ruslan@rsemenov.com>
2014-01-10 14:46:19 +0200

    Add new package for OCP site, started emails sending project

---
+++ Unico/Unico.Data/Entities/SenderEmail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unico.Data.Interfaces;
+
+namespace Unico.Data.Entities
+{
+    public class SenderEmail : IEntity
+    {
+        public virtual int SenderEmailId { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Host { get; set; }
+        public virtual int Port { get; set; }
+        public virtual bool EnableSsl { get; set; }
+        public virtual string Password { get; set; }
+    }
+
+    public class Email : IEntity
+    {
+        public virtual int EmailId { get; set; }
+        public virtual Guid AccountId { get; set; }
+        public virtual string EmailContent { get; set; }
+        public virtual int EmailTitle { get; set; }
+        public virtual DateTime SendOn { get; set; }
+    }
+}
---
+++ Unico/Unico.Email/EmailSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unico.Email
+{
+    public interface IEmailSender
+    {
+        void Send(string emailsTo, string emailFrom, string content, string title);
+    }
+
+    public class EmailSender : IEmailSender
+    {  
+        public void Send(string emailsTo, string emailFrom, string content, string title)
+        {
+            
+        }
+    }
+
+}

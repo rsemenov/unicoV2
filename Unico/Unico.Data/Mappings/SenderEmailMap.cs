daf27a5a81ae68be1cb7e682baa9538a76f2bf66
ruslan <ruslan@rsemenov.com>
2014-01-10 19:31:02 +0200

    add razor engine generator

---
+++ Unico/Unico.Data/Mappings/SenderEmailMap.cs
@@ -0,0 +1,41 @@
+﻿using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unico.Data.Entities;
+
+namespace Unico.Data.Mappings
+{
+    public class SenderEmailMap : ClassMap<SenderEmail>
+    {
+        public SenderEmailMap()
+        {
+            Table("SenderEmails");
+            Schema(DataConfiguration.SchemeName);
+
+            Id(x => x.SenderEmailId);
+            Map(x => x.Email);
+            Map(x => x.Password);
+            Map(x => x.Host);
+            Map(x => x.Port);
+            Map(x => x.EnableSsl);
+        }
+    }
+
+    public class EmailMap : ClassMap<Email>
+    {
+        public EmailMap()
+        {
+            Table("Emails");
+            Schema(DataConfiguration.SchemeName);
+
+            Id(x => x.EmailId);
+            Map(x => x.AccountId);
+            Map(x => x.EmailContent);
+            Map(x => x.EmailTitle);
+            Map(x => x.SendOn).ReadOnly();
+        }
+    }
+}
--- Unico/Unico.Email/EmailSender.cs
+++ Unico/Unico.Email/EmailSender.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Unico.Email
 {
+    public enum EmailType
+    {
+        OrderConfirmation,
+        NewOrderCreated,
+    }
+
+    public interface IEmailGenerator
+    {
+        string Generate(EmailType emailType, object model);
+    }
+
+    public class EmailGenerator : IEmailGenerator
+    {
+        public static string GetEmailTemplateFile(EmailType type)
+        {
+            return Path.Combine("./EmailTemplates/", type.ToString() + ".cshtml");
+        }
+
+        public string Generate(EmailType emailType, object model)
+        {
+            var templateFile = GetEmailTemplateFile(emailType);
+            if (!File.Exists(templateFile))
+            {
+                throw new FileNotFoundException(string.Format("Template file {0} not found", templateFile));
+            }
+
+            try
+            {
+                return RazorEngine.Razor.Parse(templateFile, model);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Razor generation exception", e);
+            }
+        }
+    }
+
+
     public interface IEmailSender
     {
         void Send(string emailsTo, string emailFrom, string content, string title);
